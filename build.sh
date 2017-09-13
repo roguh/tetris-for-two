5008086eda4fec722ed8d1204912a37f84c83681
Hugo Rivera <hugo.rivera2319@gmail.com>
2017-09-13 11:16:24 -0600

    Update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 JS_BUILD=main.js
 elm-make --warn Main.elm --output=$JS_BUILD
 closure-compiler --js $JS_BUILD --compilation_level ADVANCED_OPTIMIZATIONS --js_output_file compiled.$JS_BUILD 
-closure-compiler --js $JS_BUILD --compilation_level ADVANCED_OPTIMIZATIONS --js_output_file compiled5s.$JS_BUILD --language_in=ECMASCRIPT5_STRICT
-closure-compiler --js $JS_BUILD --compilation_level ADVANCED_OPTIMIZATIONS --js_output_file compiled5.$JS_BUILD --language_in=ECMASCRIPT5
